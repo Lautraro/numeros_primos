6f8adc8010f3167eebb67136d40e27f97839db1e
Lautraro <lautaroalejandrosaavedra2006@gmail.com>
2025-03-27 22:05:40 -0300

    Add files via upload
    Profe si puede abralo con Zinjal ya que no pude hacerlo en VScode

---
+++ primos.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+	
+	int n,primo,j;
+	
+	printf ("Ingrese un número: ");
+	scanf ("%d", &n);
+	
+	printf("Números primos del 1 al %d:\n", n);
+	
+	for (int i = 1; i <= n; i++) {
+		primo = 1;
+		if (i < 2) {
+			primo = 0;
+		} else {
+			for (j = 2; j * j <= i; j++) {
+				if (i % j == 0) {
+					primo = 0;
+					break;
+				}
+			}
+		}
+		if (primo) {
+			printf("%d ", i);
+		}
+	}
+	
+	printf("\n");
+	
+
+	
+	return 0;
+}
+
